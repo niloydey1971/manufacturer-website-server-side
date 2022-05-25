a148106be0c42947faf777013f83e3d1e63fd037
Anik-the-dev <u1304047@student.cuet.ac.bd>
2022-05-25 18:30:02 +0600

    db connected successfully

--- index.js
+++ index.js
@@ -7,3 +7,15 @@ app.use(cors())
 app.use(express.json())
 require('dotenv').config();
 const port = process.env.PORT || 4000;
+
+// database connection..............
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pegsuub.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+app.get('/', (req, res) => {
+    res.send("Running my Server!")
+})
+
+app.listen(port, () => {
+    console.log("Listening port 4000")
+})
