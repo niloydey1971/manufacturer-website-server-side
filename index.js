d437ce0689fe754baa21089bca2e1d681aa2bf8d
Anik-the-dev <u1304047@student.cuet.ac.bd>
2022-05-25 18:40:38 +0600

    data get from server

--- index.js
+++ index.js
@@ -12,6 +12,27 @@ const port = process.env.PORT || 4000;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pegsuub.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+// data CRUD...............
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('pchutCollection').collection('pcParts');
+
+        // get data from mongo db
+        app.get('/products', async (req, res) => {
+            const query = {}
+            const cursor = database.find(query)
+            const db = await cursor.toArray()
+            res.send(db)
+        })
+    } finally {
+
+    }
+
+}
+run().catch(console.dir);
+
+// check the server..............
 app.get('/', (req, res) => {
     res.send("Running my Server!")
 })
